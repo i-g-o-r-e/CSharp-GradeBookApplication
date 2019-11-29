ff687c2004c63d2bce5f66e248487e8347a16502
igore <i-g-o-r-e@users.noreply.github.com>
2019-11-29 16:38:14 +0100

    Adding types

---
+++ GradeBook/GradeBooks/StandardGradeBook .cs
@@ -0,0 +1,12 @@
+﻿namespace GradeBook.GradeBooks
+{
+    using GradeBook.Enums;
+
+    public class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+    }
+}
